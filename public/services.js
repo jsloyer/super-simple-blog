53b92bb90b1ca3b517e9274bccc1b5752759a0b8
Jeff Sloyer <jbsloyer@us.ibm.com>
2014-09-02 09:49:01 -0400

    more updates

--- public/services.js
+++ public/services.js
@@ -3,3 +3,17 @@ function getBlogPosts() {
         displayBlogPosts(data);
     });
 }
+
+function updateBlogPost(id, data) {
+    $.ajax({
+        url: '/api/post/' + id,
+        type: 'PUT',
+        data: data,
+        success: function(result) {
+            $("#edit-" + id).modal("hide");
+            $('body').removeClass('modal-open');
+            $('.modal-backdrop').remove();
+            getBlogPosts();
+        }
+    });
+}
--- public/views.js
+++ public/views.js
@@ -1,4 +1,21 @@
 function displayBlogPosts(data) {
     var posts = _.template($("#postsTemplate").html(), {items: data.posts});
-    $(".container").append(posts);
+    $(".container .posts").empty();
+    $(".container .posts").append(posts);
 }
+$(document).ready(function() {
+    $(document).on("click", "form.edit", function(e) {
+        //get the form the that was click
+        console.log(e);
+        if (e.target.className === "btn btn-primary edit") {
+            var formData = ($(e.currentTarget).serializeArray()),
+                data = {},
+                id = $(e.currentTarget).attr("data-id");
+            for (var index in formData) {
+                var field = formData[index];
+                data[field.name] = field.value;
+            }
+            updateBlogPost(id, data);
+        }
+    });
+});
